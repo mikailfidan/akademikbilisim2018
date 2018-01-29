a0e004b06b32fb45d9ca787805b45007df40fdc7
fdnmikail@gmail.com <fdnmikail@gmail.com>
2018-01-29 09:37:19 +0300

    add category

--- election/models.py
+++ election/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
-class Category(models.Model):
-   title=models.CharField('Kategori Adı', max_length=100)
-   desc=models.CharField('Kategori Açıklaması', max_length=250)
-   created_at=models.DateTimeField("Oluşturma Tarihi", null=False, blank=True, auto_now=True)
 
-   class Meta:
-    verbose_name='Kategori'
-    verbose_name_plural='Kategoriler'
+class Category(models.Model):
+    title = models.CharField('Kategori Adı', max_length=100)
+   # desc = models.CharField('Kategori Açıklaması', max_length=250)
+    created_at = models.DateTimeField("Oluşturma Tarihi", null=False, blank=True, auto_now=True)
+
+    class Meta:
+        verbose_name = 'Kategori'
+        verbose_name_plural = 'Kategoriler'
 
     def __str__(self):
         return str(self.title)
 
 
 class Survey(models.Model):
-    category = models.ForeignKey(Category, verbose_name='Kategori', null=False, blank=False)
+    category = models.ForeignKey(Category)
     name = models.CharField('Araştırma Adı', max_length=100)
-    active = models.BooleanField('Aktif mi?', null=False, blank=False, default=False)
     created_at = models.DateTimeField('Oluşturulma Tarihi', null=True, blank=True, auto_now=True)
     updated_at = models.DateTimeField('Güncellenme Tarihi', null=True, blank=True, auto_now_add=True)
+    active = models.BooleanField('Aktif mi?', null=False, blank=False, default=False)
 
     class Meta:
         verbose_name = 'Araştırma'
@@ -26,9 +27,6 @@ class Survey(models.Model):
 
     def __str__(self):
         return str(self.name)
-
-
-
 
 
 
